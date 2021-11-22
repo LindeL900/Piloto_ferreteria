ffcd3d13818ab078f2456e95a4b9a8a8e233b326
LindeL900 <58795762+LindeL900@users.noreply.github.com>
2021-11-22 16:44:44 -0300

    ejemplo basico streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#Pandas Piloto analisis ventas de un mes - leer resultado carga ya depurado
+
+import streamlit as st
+st.write('Pagina web ejemplo') 
+c1, c2 = st.columns([3, 7])
+c1.markdown("## Testo columna 1")
+#c1.image('images/avatar.jpeg', width=300)
+# Contenido de la segunda
+c2.markdown("## texto segunda columna")
+
+
+
+
