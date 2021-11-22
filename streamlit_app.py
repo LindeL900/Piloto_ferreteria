93194b96e7bc735272c311ea86e097990980dcb8
LindeL900 <58795762+LindeL900@users.noreply.github.com>
2021-11-22 19:41:23 -0300

    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ c1, c2 = st.columns([3, 7])
 c1.markdown("## Testo columna 1")
 #c1.image('images/avatar.jpeg', width=300)
 # Contenido de la segunda
-c2.markdown("## texto segunda columna")
-c2.checkbox("Un Checkbox")
-c2.select_slider("Slider de Texto", options=["uno", "dos", "tres", "cuatro"])
-
-
-
+#c2.markdown("## texto segunda columna")
+#c2.checkbox("Un Checkbox")
+#c2.select_slider("Slider de Texto", options=["uno", "dos", "tres", "cuatro"])
+#c3.radio("Radio", options=["AM", "FM", "Online"])
+c1.selectbox("¿Opciones ?", options=["Grafico x dia", "Grafico por cliente"])
+c2.multiselect("Tipo documento", options=["FCV", "BLV", "NCV"])
