2386aec77cfcc4351024d3447c73ab0bca5230c3
LindeL900 <58795762+LindeL900@users.noreply.github.com>
2022-08-31 12:31:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 #%% 1 lee acumulado y genera df mes de analisis
 #desde la posicion del programa hay que colocar los subdirectorios
