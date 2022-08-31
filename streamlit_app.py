33f02e1e086576537f0e64f69f55c56b802e098a
LindeL900 <58795762+LindeL900@users.noreply.github.com>
2022-08-31 11:45:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,15 @@
 #Pandas Piloto analisis ventas de un mes - leer resultado carga ya depurado
 
 import streamlit as st
-st.write('Pagina web ejemplo') 
+st.write('Pagina web mostrar graficos') 
 c1, c2 = st.columns([3, 7])
-c1.markdown("## Testo columna 1")
+#c1.markdown("## Parametros del grafico")
 #c1.image('images/avatar.jpeg', width=300)
 # Contenido de la segunda
 #c2.markdown("## texto segunda columna")
 #c2.checkbox("Un Checkbox")
 #c2.select_slider("Slider de Texto", options=["uno", "dos", "tres", "cuatro"])
 #c3.radio("Radio", options=["AM", "FM", "Online"])
-c1.selectbox("¿Opciones ?", options=["Grafico x dia", "Grafico por cliente"])
+c1.selectbox("Graficos a generar:", options=["Grafico x dia", "Grafico por cliente"])
 c2.multiselect("Tipo documento", options=["FCV", "BLV", "NCV"])
+
